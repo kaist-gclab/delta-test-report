bc503c03910bc36d06cf1356f83ea35375553192
Hyeonguk Ryu <ryu@hyeonguk.com>
2019-12-10 06:15:30 +0900

    테스트 전체 5종 구현을 준비합니다

---
+++ index.ts
@@ -0,0 +1,2 @@
+import { run } from './src/main';
+run();
---
+++ src/test/formats.ts
@@ -0,0 +1,8 @@
+// 3. 지원 3차원 모델 형식 개수 3개
+//     ASCII STL, BINARY STL, 자체
+//     다수 형식의 3차원 모델을 입출력하여 정상 지원 여부를 확인함.
+//     - 3차원 모델의 크기는 일반 텍스트 형식의 Wavefront OBJ를 기준으로 계산
+
+export async function run() {
+
+}
