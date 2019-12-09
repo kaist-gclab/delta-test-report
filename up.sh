75c0f4878b1b994cc56d57c9624f7ed5468ad752
Hyeonguk Ryu <ryu@hyeonguk.com>
2019-12-10 07:47:43 +0900

    데이터 디렉터리 권한 문제를 해결합니다

--- up.sh
+++ up.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
 cd "$DIR/delta-server/config" && \
-    rm -rf $DELTA_POSTGRES_DATA_DIRECTORY && \
+    sudo rm -rf $DELTA_POSTGRES_DATA_DIRECTORY && \
     mkdir -p $DELTA_POSTGRES_DATA_DIRECTORY && \
     source ./postgres.sh
 
 cd "$DIR/delta-object-storage" && \
-    rm -rf $DELTA_OBJECT_STORAGE_DATA_DIRECTORY && \
+    sudo rm -rf $DELTA_OBJECT_STORAGE_DATA_DIRECTORY && \
     mkdir -p $DELTA_OBJECT_STORAGE_DATA_DIRECTORY && \
     source ./run.sh
 
