9cfe57663c908e5cb8d10f124ee7302bd69caf3c
Hyeonguk Ryu <ryu@hyeonguk.com>
2020-12-09 15:46:20 +0900

    서비스 구축 병렬화

--- run.sh
+++ run.sh
@@ -3,11 +3,16 @@
 echo "이전 테스트에서 제거되지 않았을지도 모르는 잔여 파일 및 Docker 컨테이너를 모두 제거합니다." | tee log.txt
 ./down.sh
 
-echo "Ubuntu, Node.js, .NET을 다운로드합니다. 이 다운로드 단계는 서버 구축 시간에 포함하지 않습니다." | tee log.txt
-docker pull ubuntu:20.04
-docker pull node:14-alpine
-docker pull mcr.microsoft.com/dotnet/sdk:5.0-alpine
-docker pull mcr.microsoft.com/dotnet/runtime-deps:5.0-alpine
+echo "데이터베이스 서버, 웹 서버 등 기본적으로 필요한 이미지를 다운로드합니다. 이 단계는 서버 구축 시간에 포함하지 않습니다."
+
+docker pull ubuntu:20.04 &
+docker pull node:14-alpine &
+docker pull mcr.microsoft.com/dotnet/sdk:5.0-alpine &
+docker pull mcr.microsoft.com/dotnet/runtime-deps:5.0-alpine &
+docker pull postgres:13 &
+docker pull minio/minio &
+docker pull caddy:2-alpine &
+wait
 
 echo "서버 구축 시작 시각입니다. 지금부터 서버 구축 시간을 측정합니다." | tee log.txt
 date | tee log.txt
@@ -22,7 +27,7 @@ echo "서버 구축 완료 시각입니다." | tee log.txt
 date | tee log.txt
 
 DATABASE_INIT_DELAY="20"
-echo "데이터베이스 서버 초기화 작업이 완료될 수 있도록 $DATABASE_INIT_DELAY초 더 기다립니다." | tee log.txt
+echo "데이터베이스 서버 초기화 작업이 완료될 수 있도록 $DATABASE_INIT_DELAY 초 더 기다립니다." | tee log.txt
 sleep $DATABASE_INIT_DELAY
 
 echo "자동 테스트를 시작합니다." | tee log.txt
--- up.sh
+++ up.sh
@@ -1,29 +1,43 @@
 #!/usr/bin/env bash
 source env.sh
 
-cd "$DIR/delta-server/config" && \
-    rm -rf $DELTA_POSTGRES_DATA_DIRECTORY && \
-    mkdir -p $DELTA_POSTGRES_DATA_DIRECTORY && \
-    source ./postgres.sh
+(
+    cd "$DIR/delta-server/config" &&
+        rm -rf $DELTA_POSTGRES_DATA_DIRECTORY &&
+        mkdir -p $DELTA_POSTGRES_DATA_DIRECTORY &&
+        source ./postgres.sh
+) &
 
-cd "$DIR/delta-object-storage" && \
-    rm -rf $DELTA_OBJECT_STORAGE_DATA_DIRECTORY && \
-    mkdir -p $DELTA_OBJECT_STORAGE_DATA_DIRECTORY && \
-    source ./run.sh
+(
+    cd "$DIR/delta-object-storage" &&
+        rm -rf $DELTA_OBJECT_STORAGE_DATA_DIRECTORY &&
+        mkdir -p $DELTA_OBJECT_STORAGE_DATA_DIRECTORY &&
+        source ./run.sh
+) &
 
-cd "$DIR/delta-server/Delta" && \
-    DELTA_SERVER_RUN_OPTIONS="-p $DELTA_SERVER_PORT:80 --link delta-object-storage --link delta-postgres"
-    ./build.sh && \
-    source ./remote.sh
+(
+    cd "$DIR/delta-server/Delta" &&
+        DELTA_SERVER_RUN_OPTIONS="-p $DELTA_SERVER_PORT:80 --link delta-object-storage --link delta-postgres"
+    ./build.sh &&
+        source ./remote.sh
+) &
 
-cd "$DIR/delta-processor-null" && ./build.sh
+(
+    cd "$DIR/delta-processor-null" && ./build.sh
+) &
 
-cp "$DIR/env-web" "$DIR/delta-web/delta-web/.env"
-cd "$DIR/delta-web/delta-web" && \
-    DELTA_WEB_RUN_OPTIONS="-p $DELTA_WEB_PORT:80"
-    ./build.sh && \
-    source remote.sh
+(
+    cp "$DIR/env-web" "$DIR/delta-web/delta-web/.env" &&
+        cd "$DIR/delta-web/delta-web" &&
+        DELTA_WEB_RUN_OPTIONS="-p $DELTA_WEB_PORT:80"
+    ./build.sh &&
+        source remote.sh
+) &
 
-RENDERER_BASE_IMAGE="kaistgclab/delta-renderer-base"
-docker rmi $RENDERER_BASE_IMAGE
-docker pull $RENDERER_BASE_IMAGE
+(
+    RENDERER_BASE_IMAGE="kaistgclab/delta-renderer-base"
+    docker rmi $RENDERER_BASE_IMAGE &&
+        docker pull $RENDERER_BASE_IMAGE
+) &
+
+wait
