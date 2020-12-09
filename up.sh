24dd9e4249e4650df1060e3340b8b8d958145221
Hyeonguk Ryu <ryu@hyeonguk.com>
2020-12-09 11:53:31 +0900

    불필요한 명령 제거

--- up.sh
+++ up.sh
@@ -1,19 +1,18 @@
 #!/usr/bin/env bash
 
 cd "$DIR/delta-server/config" && \
-    sudo rm -rf $DELTA_POSTGRES_DATA_DIRECTORY && \
+    rm -rf $DELTA_POSTGRES_DATA_DIRECTORY && \
     mkdir -p $DELTA_POSTGRES_DATA_DIRECTORY && \
     source ./postgres.sh
 
 cd "$DIR/delta-object-storage" && \
-    sudo rm -rf $DELTA_OBJECT_STORAGE_DATA_DIRECTORY && \
+    rm -rf $DELTA_OBJECT_STORAGE_DATA_DIRECTORY && \
     mkdir -p $DELTA_OBJECT_STORAGE_DATA_DIRECTORY && \
     source ./run.sh
 
 cd "$DIR/delta-server/Delta" && \
     DELTA_SERVER_RUN_OPTIONS="-p $DELTA_SERVER_PORT:80 --link delta-object-storage --link delta-postgres"
     ./build.sh && \
-    docker save --output docker-image.tar delta-app-server && \
     source ./remote.sh
 
 cd "$DIR/delta-processor-null" && ./build.sh
