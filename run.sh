e220dbd80ae6612b349b279e37ea4c41c1588668
Hyeonguk Ryu <ryu@hyeonguk.com>
2020-12-09 15:52:28 +0900

    데이터베이스 서버 초기화 시간 포함

--- run.sh
+++ run.sh
@@ -23,12 +23,12 @@ echo "3차원 기하 모델 프로세싱 프레임워크를 다운로드합니�
 echo "3차원 기하 모델 프로세싱 프레임워크를 빌드합니다." | tee log.txt
 ./up.sh
 
-echo "서버 구축 완료 시각입니다." | tee log.txt
-date | tee log.txt
-
 DATABASE_INIT_DELAY="20"
 echo "데이터베이스 서버 초기화 작업이 완료될 수 있도록 $DATABASE_INIT_DELAY 초 더 기다립니다." | tee log.txt
 sleep $DATABASE_INIT_DELAY
+
+echo "서버 구축 완료 시각입니다." | tee log.txt
+date | tee log.txt
 
 echo "자동 테스트를 시작합니다." | tee log.txt
 ./test.sh
