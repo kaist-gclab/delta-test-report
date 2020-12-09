40d5a928927336806780d98dbe259d26c8479c32
Hyeonguk Ryu <ryu@hyeonguk.com>
2020-12-09 14:16:30 +0900

    서버 구축 시작 및 완료 시각 기록

--- run.sh
+++ run.sh
@@ -1,8 +1,15 @@
 #!/usr/bin/env bash
 
 ./down.sh
+
+echo "서버 구축 시작 시각입니다." | tee log.txt
+date | tee log.txt
+
 ./clone.sh
 ./up.sh
+
+echo "서버 구축 완료 시각입니다." | tee log.txt
+date | tee log.txt
 
 DATABASE_INIT_DELAY="20"
 echo "데이터베이스 서버 초기화 작업이 완료될 수 있도록 더 기다립니다."
