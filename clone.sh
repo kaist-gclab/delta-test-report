552b9a587fcce1c2e2f6c829c715efb3affecd84
Hyeonguk Ryu <ryu@hyeonguk.com>
2020-12-09 14:57:54 +0900

    테스트 모드 자동 선택

--- clone.sh
+++ clone.sh
@@ -1,11 +1,17 @@
 #!/usr/bin/env bash
 
-# git clone --depth=1 https://github.com/kaist-gclab/delta-server
-# git clone --depth=1 https://github.com/kaist-gclab/delta-object-storage
-# git clone --depth=1 https://github.com/kaist-gclab/delta-processor-null
-# git clone --depth=1 https://github.com/kaist-gclab/delta-web
+if [ $DELTA_TEST_MODE = "LOCAL" ]; then
+    rsync -r --exclude=node_modules --exclude=.git ../delta-server .
+    rsync -r --exclude=node_modules --exclude=.git ../delta-object-storage .
+    rsync -r --exclude=node_modules --exclude=.git ../delta-processor-null .
+    rsync -r --exclude=node_modules --exclude=.git ../delta-web .
+    rsync -r --exclude=node_modules --exclude=.git ../delta-renderer .
+else
+    git clone --depth=1 https://github.com/kaist-gclab/delta-server
+    git clone --depth=1 https://github.com/kaist-gclab/delta-object-storage
+    git clone --depth=1 https://github.com/kaist-gclab/delta-processor-null
+    git clone --depth=1 https://github.com/kaist-gclab/delta-web
+    git clone --depth=1 https://github.com/kaist-gclab/delta-renderer
+fi
 
-rsync -r --exclude=node_modules --exclude=.git ../delta-server .
-rsync -r --exclude=node_modules --exclude=.git ../delta-object-storage .
-rsync -r --exclude=node_modules --exclude=.git ../delta-processor-null .
-rsync -r --exclude=node_modules --exclude=.git ../delta-web .
+cd ./delta-renderer/delta-renderer-base/ && ./build.sh
