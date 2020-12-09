811cfa8c5e7a4bb13b3db7e129185866a41842da
Hyeonguk Ryu <ryu@hyeonguk.com>
2020-12-09 15:27:18 +0900

    테스트 스크립트 상세 설명 추가

--- run.sh
+++ run.sh
@@ -1,19 +1,32 @@
 #!/usr/bin/env bash
 
+echo "이전 테스트에서 제거되지 않았을지도 모르는 잔여 파일 및 Docker 컨테이너를 모두 제거합니다." | tee log.txt
 ./down.sh
 
-echo "서버 구축 시작 시각입니다." | tee log.txt
+echo "Ubuntu, Node.js, .NET을 다운로드합니다. 이 다운로드 단계는 서버 구축 시간에 포함하지 않습니다." | tee log.txt
+docker pull ubuntu:20.04
+docker pull node:14-alpine
+docker pull mcr.microsoft.com/dotnet/sdk:5.0-alpine
+docker pull mcr.microsoft.com/dotnet/runtime-deps:5.0-alpine
+
+echo "서버 구축 시작 시각입니다. 지금부터 서버 구축 시간을 측정합니다." | tee log.txt
 date | tee log.txt
 
+echo "3차원 기하 모델 프로세싱 프레임워크를 다운로드합니다. 이 다운로드 단계는 서버 구축 시간에 포함합니다." | tee log.txt
 ./clone.sh
+
+echo "3차원 기하 모델 프로세싱 프레임워크를 빌드합니다." | tee log.txt
 ./up.sh
 
 echo "서버 구축 완료 시각입니다." | tee log.txt
 date | tee log.txt
 
 DATABASE_INIT_DELAY="20"
-echo "데이터베이스 서버 초기화 작업이 완료될 수 있도록 더 기다립니다."
+echo "데이터베이스 서버 초기화 작업이 완료될 수 있도록 $DATABASE_INIT_DELAY초 더 기다립니다." | tee log.txt
 sleep $DATABASE_INIT_DELAY
 
+echo "자동 테스트를 시작합니다." | tee log.txt
 ./test.sh
+
+echo "테스트 잔여 파일 및 Docker 컨테이너를 모두 제거합니다." | tee log.txt
 ./down.sh
